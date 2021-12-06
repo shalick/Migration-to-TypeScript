aa307bde21541773e303b28812a8649d40bc1b5b
Alick Shabanovich <alikshabanovich@gmail.com>
2021-12-06 21:11:02 +0300

    fix: changed api link

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '08fb5442cc4b41a3b7e211b5078e5cba', 
         });
     }
